A10506/ceph-ci

common/options: set admin socket for client
Signed-off-by: luo.runbing <luo.runbing@zte.com.cn>

src/common/options.cc
@@ -239,12 +239,13 @@ std::vector<Option> get_global_options() {
     .add_see_also("admin_socket"),
 
     Option("admin_socket", Option::TYPE_STR, Option::LEVEL_ADVANCED)
-    .set_default("")
+    .set_default("$run_dir/$cluster-$name.$pid.$cctid.asok")
     .set_daemon_default("$run_dir/$cluster-$name.asok")
     .set_description("path for the runtime control socket file, used by the 'ceph daemon' command")
     .add_service("common"),
 
     Option("admin_socket_mode", Option::TYPE_STR, Option::LEVEL_ADVANCED)
+    .set_default("0777")
     .set_description("file mode to set for the admin socket file, e.g, '0755'")
     .add_service("common")
     .add_see_also("admin_socket"),

xiexingguo/ceph

src/common/options.cc
@@ -239,12 +239,13 @@ std::vector<Option> get_global_options() {
     .add_see_also("admin_socket"),
 
     Option("admin_socket", Option::TYPE_STR, Option::LEVEL_ADVANCED)
-    .set_default("")
+    .set_default("$run_dir/$cluster-$name.$pid.$cctid.casok")
     .set_daemon_default("$run_dir/$cluster-$name.asok")
     .set_description("path for the runtime control socket file, used by the 'ceph daemon' command")
     .add_service("common"),
 
     Option("admin_socket_mode", Option::TYPE_STR, Option::LEVEL_ADVANCED)
+    .set_default("0777")
     .set_description("file mode to set for the admin socket file, e.g, '0755'")
     .add_service("common")
     .add_see_also("admin_socket"),