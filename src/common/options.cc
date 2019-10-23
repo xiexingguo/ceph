e18fa9a673bb946f02d948d6d22d550940de0457
yanjun <yan.jun8@zte.com.cn>
2019-10-23 10:49:56 +0800

    common/options: disable OSD suiside caused by network failure
    set osd_max_markdown_count to be large enough to disable suiside caused by network failure
    
    Change-Id: I34adcf08476967d432fd7a9c54dbc3ceff897536
    Signed-off-by: yanjun <yan.jun8@zte.com.cn>

--- src/common/options.cc
+++ src/common/options.cc
@@ -1994,7 +1994,7 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("osd_max_markdown_count", Option::TYPE_INT, Option::LEVEL_ADVANCED)
-    .set_default(5)
+    .set_default(1000000)
     .set_description(""),
 
     Option("osd_peering_wq_threads", Option::TYPE_INT, Option::LEVEL_ADVANCED)
