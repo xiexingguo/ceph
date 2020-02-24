sunnyku/ceph-ci

mgr/MgrStandby: exit with 0 result code, as if we'd done an orderly shutdown
This matches the prior behavior with shutdown().  It also makes
teuthology happy.

Signed-off-by: Sage Weil <sage@redhat.com>

src/mgr/MgrStandby.cc
@@ -264,7 +264,7 @@ void MgrStandby::handle_signal(int signum)
 {
   ceph_assert(signum == SIGINT || signum == SIGTERM);
   derr << "*** Got signal " << sig_str(signum) << " ***" << dendl;
-  _exit(128 + signum);
+  _exit(0);  // exit with 0 reseult code, as if we had done an orderly shutdown
   //shutdown();
 }
 

xiexingguo/ceph

(cherry picked from commit b925be726bdd44ae3cd909fd0cd90633bf5c7de5)

src/mgr/MgrStandby.cc
@@ -209,7 +209,7 @@ void MgrStandby::handle_signal(int signum)
   Mutex::Locker l(lock);
   assert(signum == SIGINT || signum == SIGTERM);
   derr << "*** Got signal " << sig_str(signum) << " ***" << dendl;
-  _exit(128 + signum);
+  _exit(0);  // exit with 0 result code, as if we had done an orderly shutdown
   //shutdown();
 }
 