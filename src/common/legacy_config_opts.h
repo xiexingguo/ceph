sunnyku/ceph-ci

osdc: support pause io for objecter on monitor lost
Signed-off-by: yanjun <yan.jun8@zte.com.cn>

src/common/legacy_config_opts.h
@@ -420,6 +420,7 @@ OPTION(objecter_completion_locks_per_session, OPT_U64) // num of completion lock
 OPTION(objecter_inject_no_watch_ping, OPT_BOOL)   // suppress watch pings
 OPTION(objecter_retry_writes_after_first_reply, OPT_BOOL)   // ignore the first reply for each write, and resend the osd op instead
 OPTION(objecter_debug_inject_relock_delay, OPT_BOOL)
+OPTION(objecter_pause_on_mon_lost, OPT_BOOL)
 OPTION(objecter_requests_tracker_history_size, OPT_U32)   // max size of histroy to track requests done rate
 OPTION(objecter_dmc_op_cost_from_rados, OPT_BOOL)
 

src/common/options.cc
@@ -1622,6 +1622,10 @@ std::vector<Option> get_global_options() {
     .set_default(false)
     .set_description(""),
 
+    Option("objecter_pause_on_mon_lost", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
+    .set_default(false)
+    .set_description("pause io when losing connection to monintor"),
+
     Option("filer_max_purge_ops", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
     .set_default(10)
     .set_description(""),

xiexingguo/ceph

Change-Id: I96ab6fdf84cc461be587a71360f5371a48418aa5

src/common/legacy_config_opts.h
@@ -421,6 +421,7 @@ OPTION(objecter_completion_locks_per_session, OPT_U64) // num of completion lock
 OPTION(objecter_inject_no_watch_ping, OPT_BOOL)   // suppress watch pings
 OPTION(objecter_retry_writes_after_first_reply, OPT_BOOL)   // ignore the first reply for each write, and resend the osd op instead
 OPTION(objecter_debug_inject_relock_delay, OPT_BOOL)
+OPTION(objecter_pause_op_on_mon_lost, OPT_BOOL)
 OPTION(objecter_requests_tracker_history_size, OPT_U32)   // max size of histroy to track requests done rate
 OPTION(objecter_dmc_op_cost_from_rados, OPT_BOOL)
 

src/common/options.cc
@@ -1645,6 +1645,10 @@ std::vector<Option> get_global_options() {
     .set_default(false)
     .set_description(""),
 
+    Option("objecter_pause_op_on_mon_lost", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
+    .set_default(false)
+    .set_description("pause io when losing connection to monintor"),
+
     Option("filer_max_purge_ops", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
     .set_default(10)
     .set_description(""),